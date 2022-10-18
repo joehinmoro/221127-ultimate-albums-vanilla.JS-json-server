9d162a0571a8744d21a5e868bd321552e3ec4358
joehinmoro <joehinmoro@gmail.com>
2022-10-18 12:11:09 +0100

    inder err hdl

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
 const main = document.querySelector(".main");
 const content = document.querySelector(".content");
 const title = document.querySelector(".title");
+const heading = document.querySelector(".heading");
 
 const mainFunc = async () => {
-    // fetch album data
-    const response = await fetch("http://localhost:8080/albums");
-    const data = await response.json();
-    console.log(data);
+    try {
+        // fetch album data
+        const response = await fetch("http://localhost:8080/albums");
+        const data = await response.json();
+        console.log(data);
 
-    // create template
-    let template = ``;
-    data.map((album) => {
-        template += `
+        // create template
+        let template = ``;
+
+        data.map((album) => {
+            template += `
     <div class="col-sm-12 col-md-6 col-lg-4 mt-3">
         <div class="card text-bg-dark border-light mx-auto h-100 text-center">
         <div class="card-body">
@@ -29,8 +32,8 @@ const mainFunc = async () => {
             <ol class="list-group list-group-numbered list-group-flush">
         `;
 
-        album.songs.map((song) => {
-            template += `
+            album.songs.map((song) => {
+                template += `
             <li class="list-group-item bg-dark text-light border-secondary text-start d-flex justify-content-between align-items-start">
                 <div class="ms-2 me-auto">
                     ${song.name}
@@ -38,22 +41,29 @@ const mainFunc = async () => {
             <button class="badge btn btn-outline-light rounded-pill">${song.listenCount}</button>
             </li>
             `;
-        });
+            });
 
-        template += `
+            template += `
             </ol>
         </div>
         <div class="card-footer">
-            <a href="#" class="btn btn-outline-light btn-sm card-link"> Expand </a>
+            <a href="/details.html?id=${album.id}" class="btn btn-outline-light btn-sm card-link"> Expand </a>
             <a href="#" class="btn btn-outline-light btn-sm card-link"> Edit </a>
             <button class="btn btn-outline-light btn-sm card-link">Delete</button>
         </div>
     </div>
 </div>
 `;
-    });
-    content.innerHTML = template;
+        });
+        heading.textContent = "All Albums";
+        content.innerHTML = template;
+    } catch (error) {
+        console.log(error);
+        heading.textContent = "Error";
+        content.innerHTML = `<p>something went wrong :(</p>`;
+    }
 };
 
+title.textContent = "Albums";
 // await dom load
 window.addEventListener("DOMContentLoaded", () => mainFunc());
